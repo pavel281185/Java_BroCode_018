391a9f890738b7f802d71d6ca92b909808430e64
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:15:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // wrapper class
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 1.3;
+        String s = "String";
+
+        if (a == true) {
+            System.out.println("This is true");
+        }
+    }
+}
